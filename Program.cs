47cb099436dc5b5e6bf2b0986eb84e6e1dc5af8b
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-14 12:32:24 +0530

    added program.cs

---
+++ Program.cs
@@ -0,0 +1,277 @@
+﻿naaespace namespace AdressBook
+{
+    class program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to AdressBook");
+            AdressBook address = new AdressBook();
+
+            string command = "";
+
+            while (command != "exit")
+            {
+
+                Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+                command = Console.ReadLine().ToLower();
+
+                switch (command)
+                {
+                    case "add":
+
+
+                        address.AddPerson();
+                        break;
+                    case "edit":
+                        address.EditContact();
+                        break;
+                    case "remove":
+                        address.RemoveContact();
+                        break;
+                    case "display":
+                        address.Display();
+                        break;
+                    case "multipleperson":
+                        address.AddMultipleContact();
+                        break;
+
+
+
+                }
+            }
+        }
+    }
+}
+Footernamespace AdressBook
+{
+    class program
+{
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to AdressBook");
+        AdressBook address = new AdressBook();
+
+        string command = "";
+
+        while (command != "exit")
+        {
+
+            Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+            command = Console.ReadLine().ToLower();
+
+            switch (command)
+            {
+                case "add":
+
+
+                    address.AddPerson();
+                    break;
+                case "edit":
+                    address.EditContact();
+                    break;
+                case "remove":
+                    address.RemoveContact();
+                    break;
+                case "display":
+                    address.Display();
+                    break;
+                case "multipleperson":
+                    address.AddMultipleContact();
+                    break;
+
+
+
+            }
+        }
+    }
+}
+}
+Footernamespace AdressBook
+{
+    class program
+{
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to AdressBook");
+        AdressBook address = new AdressBook();
+
+        string command = "";
+
+        while (command != "exit")
+        {
+
+            Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+            command = Console.ReadLine().ToLower();
+
+            switch (command)
+            {
+                case "add":
+
+
+                    address.AddPerson();
+                    break;
+                case "edit":
+                    address.EditContact();
+                    break;
+                case "remove":
+                    address.RemoveContact();
+                    break;
+                case "display":
+                    address.Display();
+                    break;
+                case "multipleperson":
+                    address.AddMultipleContact();
+                    break;
+
+
+
+            }
+        }
+    }
+}
+}
+Footernamespace AdressBook
+{
+    class program
+{
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to AdressBook");
+        AdressBook address = new AdressBook();
+
+        string command = "";
+
+        while (command != "exit")
+        {
+
+            Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+            command = Console.ReadLine().ToLower();
+
+            switch (command)
+            {
+                case "add":
+
+
+                    address.AddPerson();
+                    break;
+                case "edit":
+                    address.EditContact();
+                    break;
+                case "remove":
+                    address.RemoveContact();
+                    break;
+                case "display":
+                    address.Display();
+                    break;
+                case "multipleperson":
+                    address.AddMultipleContact();
+                    break;
+
+
+
+            }
+        }
+    }
+}
+}
+Footernamespace AdressBook
+{
+    class program
+{
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to AdressBook");
+        AdressBook address = new AdressBook();
+
+        string command = "";
+
+        while (command != "exit")
+        {
+
+            Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+            command = Console.ReadLine().ToLower();
+
+            switch (command)
+            {
+                case "add":
+
+
+                    address.AddPerson();
+                    break;
+                case "edit":
+                    address.EditContact();
+                    break;
+                case "remove":
+                    address.RemoveContact();
+                    break;
+                case "display":
+                    address.Display();
+                    break;
+                case "multipleperson":
+                    address.AddMultipleContact();
+                    break;
+
+
+
+            }
+        }
+    }
+}
+}
+Footernamespace AdressBook
+{
+    class program
+{
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to AdressBook");
+        AdressBook address = new AdressBook();
+
+        string command = "";
+
+        while (command != "exit")
+        {
+
+            Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+            command = Console.ReadLine().ToLower();
+
+            switch (command)
+            {
+                case "add":
+
+
+                    address.AddPerson();
+                    break;
+                case "edit":
+                    address.EditContact();
+                    break;
+                case "remove":
+                    address.RemoveContact();
+                    break;
+                case "display":
+                    address.Display();
+                    break;
+                case "multipleperson":
+                    address.AddMultipleContact();
+                    break;
+
+
+
+            }
+        }
+    }
+}
+}
+Footer
